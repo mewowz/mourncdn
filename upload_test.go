8972889134aa04558009f7d633f5a1c7749c8e80
mewowz <mewowz@lainx.org>
2026-08-31 17:53:22 -0700

    upload: add tests for LocalAssetUploader.ServeHTTP

--- upload_test.go
+++ upload_test.go
@@ -3,13 +3,18 @@ package main
 import (
 	"bytes"
 	"crypto/sha512"
+	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"io"
 	"net/http"
+	"net/http/httptest"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
+
+	"github.com/gabriel-vasile/mimetype"
 )
 
 func TestNewLocalAssetUploader(t *testing.T) {
@@ -242,6 +247,194 @@ func TestLocalAssetUploader_writeAssetToDisk(t *testing.T) {
 
 			if err := os.Remove(got.Path); err != nil {
 				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func TestLocalAssetUploader_moveAssetToOutputDir(t *testing.T) {
+	pngSig := []byte{
+		0x89, 'P', 'N', 'G',
+		0x0D, 0x0A, 0x1A, 0x0A,
+	}
+
+	tests := []struct {
+		name            string
+		fileData        []byte
+		fileDataHashsum [sha512.Size]byte
+		extension       string
+	}{
+		{
+			"png data properly hashed and named",
+			pngSig,
+			sha512.Sum512(pngSig),
+			".png",
+		},
+		{
+			"arbitrary bytes have no ext",
+			[]byte{0x1, 0x2, 0x3, 0x4},
+			sha512.Sum512([]byte{0x1, 0x2, 0x3, 0x4}),
+			"",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			tmpDirPath := t.TempDir()
+			outputDirPath := t.TempDir()
+			uploader := &LocalAssetUploader{
+				tmpDirPath:    tmpDirPath,
+				outputDirPath: outputDirPath,
+			}
+
+			tempFilePath := filepath.Join(tmpDirPath, "testfile")
+			f, err := os.Create(tempFilePath)
+			if err != nil {
+				t.Fatalf("create: %v", err)
+			}
+
+			_, err = io.Copy(f, bytes.NewReader(test.fileData))
+			f.Close()
+			if err != nil {
+				t.Fatalf("write: %v", err)
+			}
+
+			expectedBaseName := hex.EncodeToString(test.fileDataHashsum[:]) + test.extension
+			expectedOutputFilePath := filepath.Join(outputDirPath, expectedBaseName)
+
+			hash := sha512.New()
+			_, err = io.Copy(hash, bytes.NewReader(test.fileData))
+			if err != nil {
+				t.Fatalf("hash: %v", err)
+			}
+
+			mime := mimetype.Detect(test.fileData)
+			meta := assetMeta{
+				Hash:     hash,
+				MimeInfo: mime,
+				Path:     tempFilePath,
+			}
+
+			gotOutputPath, err := uploader.moveAssetToOutputDir(meta)
+			if err != nil {
+				t.Fatalf("move asset: %v", err)
+			}
+
+			if gotOutputPath != expectedOutputFilePath {
+				t.Errorf(
+					"got outputPath=%q, want %q",
+					gotOutputPath,
+					expectedOutputFilePath,
+				)
+			}
+		})
+	}
+}
+
+func TestLocalAssetUploader_ServeHTTP(t *testing.T) {
+	urlPrefix := "/assets/"
+	target := "localhost:8983/upload"
+
+	type statusCreatedResponse struct {
+		AssetPath string `json:"assetpath"`
+	}
+
+	maxUploadSize := 10
+
+	tests := []struct {
+		name               string
+		method             string
+		body               []byte
+		expectedStatusCode int
+		expectedHeaders    map[string]string
+	}{
+		{
+			"upload succeeds normally",
+			"POST",
+			[]byte{0x0, 0x01},
+			http.StatusCreated,
+			map[string]string{"Content-Type": "application/json"},
+		},
+		{
+			"non-POST method returns 405",
+			"GET",
+			[]byte{},
+			http.StatusMethodNotAllowed,
+			make(map[string]string),
+		},
+		{
+			"oversized upload returns 413",
+			"POST",
+			bytes.Repeat([]byte{0x0}, maxUploadSize+1),
+			http.StatusRequestEntityTooLarge,
+			make(map[string]string),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			tmpDir := t.TempDir()
+			outputDir := t.TempDir()
+
+			uploader, err := NewLocalAssetUploader(
+				tmpDir,
+				outputDir,
+				urlPrefix,
+				int64(maxUploadSize),
+			)
+			if err != nil {
+				t.Fatalf("LocalAssetUploader: %v", err)
+			}
+
+			req := httptest.NewRequest(
+				test.method,
+				target,
+				bytes.NewReader(test.body),
+			)
+			w := httptest.NewRecorder()
+			uploader.ServeHTTP(w, req)
+
+			resp := w.Result()
+
+			if resp.StatusCode != test.expectedStatusCode {
+				t.Fatalf(
+					"got respStatusCode=%d, want %d",
+					resp.StatusCode,
+					test.expectedStatusCode,
+				)
+			}
+			if test.method != "POST" {
+				allowStr := resp.Header.Get("Allow")
+				if allowStr != "POST" {
+					t.Fatalf("want \"Allow: POST\", got %v", allowStr)
+				}
+				return // end test early
+			}
+
+			if val, ok := test.expectedHeaders["Content-Type"]; ok {
+				got := resp.Header.Get("Content-Type")
+				if got != val {
+					t.Fatalf("want %v, got %v", val, got)
+				}
+
+				dec := json.NewDecoder(w.Body)
+				dec.DisallowUnknownFields()
+				var jsonResp statusCreatedResponse
+				err = dec.Decode(&jsonResp)
+				if err != nil {
+					t.Fatalf("unmarshal: %v", err)
+				}
+
+				baseName, hasPrefix := strings.CutPrefix(
+					jsonResp.AssetPath,
+					urlPrefix,
+				)
+				if !hasPrefix {
+					t.Fatalf("want prefix %q, got %q", urlPrefix, jsonResp.AssetPath)
+				}
+				if baseName == "" {
+					t.Fatalf("want string after urlPrefix, got %q", jsonResp.AssetPath)
+				}
 			}
 		})
 	}
