ff12b860b04304632f751e9d07f38a0c847f94cc
mewowz <mewowz@lainx.org>
2026-09-03 01:47:33 -0700

    auth: add status 501 for the auth stub

--- auth.go
+++ auth.go
@@ -7,6 +7,9 @@ import (
 func HTTPAuthenticator(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// stub
+		http.Error(w, "auth not implemented", http.StatusNotImplemented)
+		return
+		// end stub
 
 		next.ServeHTTP(w, r)
 	})
