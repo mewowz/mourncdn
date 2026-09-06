dc2022317ae8426b161ad6ddd3d56af264eae5f8
mewowz <mewowz@lainx.org>
2026-09-05 21:27:52 -0700

    main: add main with full server setup & teardown

--- main.go
+++ main.go
@@ -1,4 +1,72 @@
 package main
 
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const DefaultConfigFilePath = "./config.yml"
+
 func main() {
+	logger := slog.Default()
+	if err := run(logger); err != nil {
+		os.Exit(1)
+	}
+}
+
+func run(logger *slog.Logger) error {
+	config, err := LoadConfigFile(DefaultConfigFilePath)
+	if err != nil {
+		logger.Error("config", "err", err)
+		return err
+	}
+
+	server, err := NewCDNServer(
+		config.ServeCfg,
+		config.UploadCfg,
+		config.HTTPCfg,
+		logger,
+	)
+	if err != nil {
+		logger.Error("cdn server", "err", err)
+		return err
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- server.Start()
+	}()
+
+	select {
+	case err := <-errCh:
+		if err != nil {
+			logger.Error("server", "err", err)
+		}
+		return err
+	case <-c:
+		logger.Info("shutting down server gracefully")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	if err := server.ShutdownGracefully(ctx); err != nil {
+		logger.Error("graceful shutdown failed", "err", err)
+
+		if err := server.ForceCloseServer(); err != nil {
+			logger.Error("force close failed", "err", err)
+		}
+	}
+
+	<-errCh
+	logger.Info("bye")
+
+	return nil
 }
